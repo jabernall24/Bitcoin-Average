7a3f3a896b44f8f2db16942246c2684584de16bd
jabernall24 <jabernall24@gmail.com>
2018-12-08 14:13:40 -0800

    Initial Commit

---
+++ Bitcoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin
+//
+//  Created by Jesus Andres Bernal Lopez on 12/8/18.
+//  Copyright © 2018 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
